82a91342f4105ca6b0db6e0ec5e9ca813ba65698
pashkovka115 <pariss8@mail.ru>
2025-03-28 11:59:36 +0300

    Add vuex

--- src/main.js
+++ src/main.js
@@ -4,7 +4,9 @@ import App from './App.vue'
 import routes from "@/routes";
 
 import '@/assets/scss/main.scss'
+import {store} from "@/store";
 
 const app = createApp(App)
 app.use(routes);
+app.use(store);
 app.mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,23 @@
+import {createStore} from "vuex";
+
+export const store = createStore({
+    state: {
+        item: null,
+        items: []
+    },
+    mutations:{
+        setItem(state, payload){
+
+        }
+    },
+    actions:{
+        setItem(context, payload){
+
+        }
+    },
+    getters:{
+        getItem(state, payload){
+
+        }
+    }
+})
